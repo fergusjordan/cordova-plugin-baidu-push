e70797e522a9221ab60478f6cae3c8eff257185c
Ian McCrum <ianmccrum@gmail.com>
2017-02-27 20:19:36 +0800

    Fix missing import

--- src/android/BaiduPushReceiver.java
+++ src/android/BaiduPushReceiver.java
@@ -11,6 +11,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.util.Log;
 
